1078bcf1322a8ce6ffcc84a255b3f68111bed1f1
mcamilx <melomarinmariacamilagmail.com>
2025-04-02 10:00:21 -0500

    lombok

--- proyecto_banco/src/main/java/module-info.java
+++ proyecto_banco/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module co.edu.uniquindio.proyecto_banco {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    .
 
 
     opens co.edu.uniquindio.proyecto_banco to javafx.fxml;
